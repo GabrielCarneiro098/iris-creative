2d87af81c7806f67f58a29a64134787d34a29981
GabrielCarneiro098 <gabrielcarneiro098gc@gmail.com>
2022-11-17 01:29:28 -0300

    new commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.section-1', {duration: 2000});
+sr.reveal('.section-2', {duration: 1000});
+sr.reveal('.section-3', {duration: 1000});
+sr.reveal('.section-4', {duration: 1000});
+sr.reveal('.article-1', {duration: 1000});
+sr.reveal('.article-2', {duration: 1000});
+sr.reveal('.article-3', {duration: 1000});
+sr.reveal('.article-4', {duration: 1000});
