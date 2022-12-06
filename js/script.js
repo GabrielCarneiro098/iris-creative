d0cb57ca92c9f832bd5bbc2033477e855c7c6a7d
GabrielCarneiro098 <gabrielcarneiro098gc@gmail.com>
2022-12-06 09:46:44 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -7,15 +7,12 @@ sr.reveal('.paragraph-2', {duration: 1500});
 sr.reveal('.this-project', {duration: 1500});
 sr.reveal('.full-portfolio', {duration: 1500});
 sr.reveal('.paragraph-3', {duration: 1500});
+sr.reveal('.fade-born', {duration: 1500});
 sr.reveal('.our-services', {duration: 1500});
-sr.reveal('.section-4', {duration: 1500});
+
 sr.reveal('.article-1', {duration: 1500});
 sr.reveal('.article-2', {duration: 1500});
 sr.reveal('.article-3', {duration: 1500});
 sr.reveal('.article-4', {duration: 1500});
-sr.reveal('.imagem-4', {duration: 1500});
-sr.reveal('.imagem-5', {duration: 1500});
-sr.reveal('.imagem-6', {duration: 1500});
-sr.reveal('.imagem-7', {duration: 1500});
-sr.reveal('.fade-born', {duration: 1500});
+
 sr.reveal('.instagram', {duration: 1500});
